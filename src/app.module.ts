93d75575753aad519a41d012f9d2042fbe1365ba
Vierry Fadhilah <vierryfadhillah9@gmail.com>
2023-04-11 17:49:19 +0700

    update module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
-// import { PaymentModule } from './payment/payment.module';
-// import { HotelModule } from './hotel-schema/hotel.module';
-// import { MasterModule } from './master-schema/master.module';
-// import { HrModule } from './hr/hr.module';
+import { PaymentModule } from './payment/payment.module';
+import { HotelModule } from './hotel-schema/hotel.module';
+import { MasterModule } from './master-schema/master.module';
+import { HrModule } from './hr/hr.module';
 import { RestoTModule } from './resto/resto-t.module';
 import { PurchasingModule } from './purchasing/purchasing.module';
-// import { UsersTModule } from './users/users.module';
+import { UsersTModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -22,12 +22,13 @@ import { PurchasingModule } from './purchasing/purchasing.module';
       synchronize: true,
     }),
     UsersTModule,
-    // HotelModule,
-    // HrModule,
+    HotelModule,
+    HrModule,
     RestoTModule,
     PurchasingModule,
-    // UsersTModule,
-    // MasterModule,
+
+    MasterModule,
+    PaymentModule,
   ],
 })
 export class AppModule {}
