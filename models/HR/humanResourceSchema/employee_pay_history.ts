d94dd97b6e5f4c955a874a59186f67e3df40b8f8
Vierry Fadhilah <vierryfadhillah9@gmail.com>
2023-04-13 14:02:24 +0700

    fix employee input, revisi get for update

--- models/HR/humanResourceSchema/employee_pay_history.ts
+++ models/HR/humanResourceSchema/employee_pay_history.ts
@@ -29,7 +29,7 @@ export class employee_pay_history
 {
   @ForeignKey(() => employee)
   @Column({ primaryKey: true, type: DataType.INTEGER })
-  @Index({ name: 'pk_ephi', using: 'btree', unique: true })
+  @Index({ name: 'pk_ephi', using: 'btree', unique: false })
   ephi_emp_id!: number;
 
   @Column({ primaryKey: true, type: DataType.DATE(6) })
